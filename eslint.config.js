bbf7e085f319cfb7f883fcde6fdde0b7613901e4
Вадим Кулишов <vadimkulisov@MacBook-Pro-Vadim.local>
2025-02-18 02:30:30 +0500

    Исправлены ошибки ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from '@eslint/js';
+import globals from 'globals';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'off',
+      'import/extensions': 'off',
+    },
+  },
 ];
